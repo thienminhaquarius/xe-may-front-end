b26e64e3059f302ccc3ce804b9112043998b731b
Thiện Minh <32061193+thienminhaquarius@users.noreply.github.com>
2019-03-02 22:23:28 +0700

    upload image

--- src/app/add-new-bike/add-new-bike.component.ts
+++ src/app/add-new-bike/add-new-bike.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+class newBike {
+  name: string
+  price: number
+  // thumbnailImage: any
+
+  info: string
+  // detailImg1: any
+  // detailImg2: any
+  // detailImg3: any
+
+}
+
 @Component({
   selector: 'app-add-new-bike',
   templateUrl: './add-new-bike.component.html',
   styleUrls: ['./add-new-bike.component.css']
 })
 export class AddNewBikeComponent implements OnInit {
+  public messages: string = '';
+  public newBike = new newBike
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createBike() {
+    console.log(this.newBike);
   }
 
 }
